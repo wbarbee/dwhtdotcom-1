bc3ec6fa8c3eafa8cbff38150609720b0c0ed5e9
Will Barbee <will.clayton.barbee@gmail.com>
2024-10-14 16:33:52 -0500

    fix build

--- utils/formatCurrentEventData.ts
+++ utils/formatCurrentEventData.ts
@@ -18,7 +18,7 @@ export function formatCurrentEventData(event: Partial<Game>): Game {
 		location: event.location ?? 'unknown',
 		date: event.date ?? '',
 		timestamp: event.timestamp ?? 0,
-		status: event.status ?? '',
+		status: event.status ?? undefined,
 		result: event.result ?? 'upcoming',
 		isTexasHome: event.isTexasHome ?? false,
 	};
