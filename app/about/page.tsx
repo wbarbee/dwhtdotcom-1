158a9a827cc459070178d874f3f965d62d8ca8dc
Will Barbee <will.clayton.barbee@gmail.com>
2024-10-15 12:16:08 -0500

    chore: remove placeholder blur

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -8,13 +8,7 @@ export default function About() {
 		<div className='about-page h-full flex items-center justify-center'>
 			<div className='bg-background/60 dark:bg-default-100/50 h-auto max-w-[810px] w-[95%] flex flex-col px-6 pb-6 -mt-[4rem] animate-fade-in'>
 				<div className='max-w-[5rem] w-full h-auto mx-auto my-8'>
-					<Image
-						alt='longhorn logo'
-						src={hookSrc}
-						width={100}
-						height={100}
-						placeholder='blur'
-					/>
+					<Image alt='longhorn logo' src={hookSrc} width={100} height={100} />
 				</div>
 				<p className='mb-6 font-menlo'>
 					didwehookthem.com seeks to become the premier authority on whether or
