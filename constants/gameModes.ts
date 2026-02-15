b88bcb9bce054942aa8a0e1f7caa516f6739d3d0
wbarbee <will.clayton.barbee@gmail.com>
2026-02-15 14:06:19 -0800

    fix: emoji sizing
    Signed-off-by: wbarbee <will.clayton.barbee@gmail.com>

--- constants/gameModes.ts
+++ constants/gameModes.ts
@@ -3,37 +3,37 @@ const gameModes = {
 		backgroundImage: '/images/gwen.jpg',
 		backgroundImageNight: '/images/gwen.jpg',
 		title: 'OFF SEASON',
-		hookEmClasses: 'text-6xl background-position-[-400px] top-[50px]',
+		hookEmClasses: 'text-7xl md:text-6xl background-position-[-400px] top-[50px]',
 	},
 	win: {
 		backgroundImage: '/images/hank.png',
 		backgroundImageNight: '/images/hank.png',
 		title: 'We hooked them.',
-		hookEmClasses: 'text-6xl',
+		hookEmClasses: 'text-7xl md:text-6xl',
 	},
 	loss: {
 		backgroundImage: '/images/hank-loss.webp',
 		backgroundImageNight: '/images/hank-loss.webp',
 		title: 'We did not hook them',
-		hookEmClasses: 'text-6xl rotate-180',
+		hookEmClasses: 'text-7xl md:text-6xl rotate-180',
 	},
 	upcoming: {
 		backgroundImage: '/images/crowd.png',
 		backgroundImageNight: '/images/crowd.png',
 		title: 'UPCOMING...',
-		hookEmClasses: 'text-6xl',
+		hookEmClasses: 'text-7xl md:text-6xl',
 	},
 	current: {
 		backgroundImage: '/images/dkr-day.webp',
 		backgroundImageNight: '/images/dkr-night.webp',
 		title: '',
-		hookEmClasses: 'text-6xl animate-spin',
+		hookEmClasses: 'text-7xl md:text-6xl animate-spin',
 	},
 	auto: {
 		backgroundImage: '',
 		backgroundImageNight: '',
 		title: '',
-		hookEmClasses: 'text-6xl',
+		hookEmClasses: 'text-7xl md:text-6xl',
 	},
 };
 
